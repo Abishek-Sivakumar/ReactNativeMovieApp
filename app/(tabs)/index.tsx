f18b7384163cf50c971028f9b4a9e81d0708f410
ABISHEK <abideepu19.vrs@gmail.com>
2025-06-01 11:53:34 +0530

    Implemeting FlaList in Home Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,30 @@
-import { Image, ScrollView, Text, View } from 'react-native'
+import {
+    ActivityIndicator,
+    FlatList,
+    Image,
+    ScrollView,
+    Text,
+    View,
+} from 'react-native'
 import { images } from '@/constants/images'
 import { icons } from '@/constants/icons'
 import SearchBar from '@/components/SearchBar'
 import { useRouter } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
 
 export default function Index() {
     const router = useRouter()
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError,
+    } = useFetch(() =>
+        fetchMovies({
+            query: '',
+        })
+    )
+
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className="absolute w-full z-0"></Image>
@@ -18,14 +37,38 @@ export default function Index() {
                     source={icons.logo}
                     className="w-12 h-10 mx-auto mt-20 mb-5"
                 ></Image>
-                <View className="mt-5">
-                    <SearchBar
-                        onPress={() => {
-                            router.push('/search')
-                        }}
-                        placeHolder="Search for Movies"
-                    ></SearchBar>
-                </View>
+                {moviesLoading ? (
+                    <ActivityIndicator
+                        size="large"
+                        color="#0000ff"
+                        className="mt-10 self-center"
+                    />
+                ) : moviesError ? (
+                    <Text>Error: {moviesError?.message}</Text>
+                ) : (
+                    <View className="mt-5">
+                        <SearchBar
+                            onPress={() => {
+                                router.push('/search')
+                            }}
+                            placeHolder="Search for Movies"
+                        ></SearchBar>
+                        <>
+                            <Text className="text-lg text-white font-bold mt-5 mb-3">
+                                Latest Movies
+                            </Text>
+                            <FlatList
+                                data={movies}
+                                renderItem={({ item }) => (
+                                    <Text className="text-white text-sm">
+                                        {item.title}
+                                    </Text>
+                                )}
+                                scrollEnabled={false}
+                            />
+                        </>
+                    </View>
+                )}
             </ScrollView>
         </View>
     )
--- services/useFetch.ts
+++ services/useFetch.ts
@@ -2,21 +2,27 @@ import { useState, useEffect } from 'react'
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
     const [data, setData] = useState<T | null>(null)
-    const [error, setError] = useState<T | null>(null)
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<Error | null>(null)
+
     const fetchData = async () => {
         try {
             setLoading(true)
             setError(null)
+
             const result = await fetchFunction()
             setData(result)
         } catch (err) {
-            // @ts-ignore
-            setError(err instanceof Error ? err : new Error('Unknown error'))
+            setError(
+                err instanceof Error
+                    ? err
+                    : new Error('An unknown error occurred')
+            )
         } finally {
             setLoading(false)
         }
     }
+
     const reset = () => {
         setData(null)
         setError(null)
@@ -28,7 +34,8 @@ const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
             fetchData()
         }
     }, [])
-    return { data, loading, error, refetch: fetchData(), reset }
+
+    return { data, loading, error, refetch: fetchData, reset }
 }
 
 export default useFetch
