48a1a37ac21617c21cc376f58af05fb2f8835cdf
ABISHEK <abideepu19.vrs@gmail.com>
2025-06-02 23:28:47 +0530

    Designed the search functionality

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,4 +1,5 @@
-import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import { View, Image, FlatList, ActivityIndicator } from 'react-native'
+import { Text } from 'react-native'
 import { images } from '@/constants/images'
 import MovieCard from '@/components/MovieCard'
 import useFetch from '@/services/useFetch'
@@ -6,18 +7,35 @@ import { fetchMovies } from '@/services/api'
 import { useRouter } from 'expo-router'
 import { icons } from '@/constants/icons'
 import SearchBar from '@/components/SearchBar'
+import { useEffect, useState } from 'react'
 
 const Search = () => {
     const router = useRouter()
+    const [searchQuery, setSearchQuery] = useState('')
     const {
         data: movies,
         loading,
         error,
-    } = useFetch(() =>
-        fetchMovies({
-            query: '',
-        })
+        refetch: loadMovies,
+        reset,
+    } = useFetch(
+        () =>
+            fetchMovies({
+                query: searchQuery,
+            }),
+        false
     )
+
+    useEffect(() => {
+        const timeoutId = setTimeout(async () => {
+            if (searchQuery.trim()) {
+                await loadMovies()
+            } else {
+                reset()
+            }
+        }, 500)
+        return () => clearTimeout(timeoutId)
+    }, [searchQuery])
 
     return (
         <View className="flex-1 bg-primary">
@@ -40,13 +58,35 @@ const Search = () => {
                 contentContainerStyle={{
                     paddingBottom: 100,
                 }}
+                ListEmptyComponent={
+                    !loading && !error ? (
+                        <View className="mt-10 px-5">
+                            <Text
+                                style={{
+                                    color: '#6B707D',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                {searchQuery.trim()
+                                    ? 'No Results found'
+                                    : 'Search for movies'}
+                            </Text>
+                        </View>
+                    ) : null
+                }
                 ListHeaderComponent={
                     <>
                         <View className="w-full flex-row justify-center mt-20">
                             <Image source={icons.logo} className="w-12 h-10" />
                         </View>
                         <View className="my-5 mt-5">
-                            <SearchBar placeHolder="Search for Movies..." />
+                            <SearchBar
+                                placeHolder="Search for Movies..."
+                                value={searchQuery}
+                                onChangeText={(text) => {
+                                    setSearchQuery(text)
+                                }}
+                            />
                         </View>
                         {loading && (
                             <ActivityIndicator
@@ -66,8 +106,8 @@ const Search = () => {
                             movies?.length > 0 && (
                                 <Text className="text-xl text-white font-bold">
                                     Search Results for {''}
-                                    <Text className="text-accent">
-                                        SEARCH TERM
+                                    <Text style={{ color: '#A091E0' }}>
+                                        {searchQuery}
                                     </Text>
                                 </Text>
                             )}
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,9 +4,11 @@ import { icons } from '@/constants/icons'
 interface Props {
     onPress?: () => void
     placeHolder: string
+    value: string
+    onChangeText: (text: string) => void
 }
 
-const SearchBar = ({ onPress, placeHolder }: Props) => {
+const SearchBar = ({ onPress, placeHolder, value, onChangeText }: Props) => {
     return (
         <View className="flex-row items-center px-5 py-4 bg-dark-200 rounded-full">
             <Image
@@ -19,8 +21,8 @@ const SearchBar = ({ onPress, placeHolder }: Props) => {
                 onPress={onPress}
                 placeholder={placeHolder}
                 placeholderTextColor="#a8b5db"
-                value=""
-                onChangeText={() => {}}
+                value={value}
+                onChangeText={onChangeText}
                 className="ml-5 text-white"
             ></TextInput>
         </View>
